43b07aa8a4fe8d5aebb6bbc8cf6f8ea3ab641a1c
Yusuf Erem <ysferem@gmail.com>
2021-11-18 16:22:37 +0300

    JAVA1 added

---
+++ JAVA1/src/Baslangıc.java
@@ -0,0 +1,42 @@
+
+public class Baslangýc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * byte a = 10; System.out.println(a); short b= 25 ; System.out.println(b);
+		 * 
+		 * double c = 100000; System.out.println("double deðeri :"+ c);
+		 * 
+		 * c = 1100; System.out.println(c);
+		 * 
+		 * 
+		 * char karakter = 15; char karakter2 = 16; char karakter3 = 17;
+		 * 
+		 * System.out.println(karakter + karakter2 + karakter3);
+		 */
+
+		//boolean yanlýs = false;
+		//System.out.println(yanlýs);
+
+
+		//char a = 'm';
+		//String str = "merhaba dunya ";
+			//	System.out.println(str + a);
+				
+		//mod alma %
+		//eþitlik ==
+		/* eþit deðil !=
+		 * ve &&
+		 * Veya ||
+		 * deðil !(a&&b)
+		 * 
+		 */
+		
+		
+		
+		
+	}
+
+}
---
+++ JAVA1/src/Operatorler.java
@@ -0,0 +1,62 @@
+
+public class Operatorler {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		int toplam = a+ b;
+		int cikarma = a-b;
+		int bolme = a/b;
+		int carpma = a*b;
+		int mod = a % b;
+		
+		
+		//System.out.println("çarpma "+ carpma +" bolme : " + bolme );
+		//System.out.println("mod " + mod);
+		//carpma++;
+		//System.out.println(carpma);
+		
+		int sayý1 = 10;
+	    int sayý2 = 20;
+		
+		boolean kosul = (sayý1 == sayý2);
+		System.out.println("Eþit mi ?" + kosul);
+		boolean kosul2 = (sayý1 / sayý2) == 0;// int yazdýðým doðru ama gerçekte yanlýþ  çünki double yapmam gerek 0.5 için
+		System.out.println("bölümü 0 a eþitmi " + kosul2);
+		boolean sonuc2 = kosul && kosul2;
+		System.out.println(sonuc2);          //&& ve anlamýna gelir ,mantýkta ikiside doðru yani 1 olmadýkça doðru olmaz
+		boolean sonuc3 = kosul || kosul2;
+
+		System.out.println(sonuc3); // || veya anlamýna gelir ve ikiside 0 yani yanlýþ olmadýkça yanlýþ olmaz
+
+		
+		boolean sonuc4 = (kosul) ? true : false ;
+		System.out.println("sonuç 4 :"+ sonuc4);
+		boolean sonuc5 = (kosul2) ? true : false ;// buranýn mantýðý doðru ise sol yanlýþ ise sað
+		System.out.println("sonuc5 :"+ sonuc5);
+		// AYNI MANTIK AÞÞAÐIDA DA GEÇERLÝ SADECE BOOLEAN DEÐÝLDE STRÝNG TANIMLADIM VE DOÐRU YANLIÞ YAPTIM
+		String sonuc6 = (kosul)? "doðru": "yanlýþ";
+		System.out.println("kosulul doðrumu yanlýþ mý ? = "+ sonuc6);
+		//SON ÖRNEK       UNUTMA SOLDAKÝ DOÐRU SAÐDAKÝ YANLIÞ ANLAMINA GELÝR
+		String sonuc7 = (a==b)? "doðru":"yanlýþ";
+		System.out.println(sonuc7);
+		
+		
+		//ATAMA ÝÞLEMLERÝ
+		//sayý1 = sayý1 +2 ; aynýsýdýr sayý1 += 2
+		sayý1 = sayý1 + 2;
+		System.out.println(sayý1);
+		sayý2 += 2;
+		System.out.println(sayý2);
+		//
+		
+
+		
+		
+		
+		
+		
+	}
+
+}
